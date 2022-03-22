cab45b9786105921863f29ac30b7064a1bed31bb
yaozeyu3 <yaozeyu3@jd.com>
2022-03-22 21:12:28 +0800

    demo2

---
+++ src/main/java/com/atguigu/demo/Util/NioExample.java
@@ -0,0 +1,25 @@
+package com.atguigu.demo.Util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioExample {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fin = new FileInputStream("/Users/yaozeyu/Desktop/党员微积分/demo/src/Test.txt");
+        FileChannel readChannel = fin.getChannel();
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        int result = readChannel.read(readBuffer);
+        System.out.println("file read successfully" + result);
+        FileOutputStream fout = new FileOutputStream("/Users/yaozeyu/Desktop/党员微积分/demo/src/dest.txt");
+        FileChannel writeChannel = fout.getChannel();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+        String message = "this is a test string";
+        writeBuffer.put(message.getBytes());
+        writeBuffer.flip();
+        writeChannel.write(writeBuffer);
+        System.out.println("file written successfully");
+    }
+}
